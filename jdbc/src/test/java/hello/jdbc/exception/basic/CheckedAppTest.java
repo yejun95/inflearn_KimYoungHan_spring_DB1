8dd287bb4b7041891fd9fc387bf86252f5480393
xodlf100 <xodlf100@gmail.com>
2024-10-04 01:08:00 +0900

    체크 예외 문제점 테스트

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,53 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+/**
+ * 체크 예외 문제점
+ * - 처리하지 못하는 예외에 대해서 계속 던지게 됨
+ * - 의존 문제 : Exception을 의존하게 됨 -> SQLException을 JPAException으로 변경 필요 시 모든 코드 변경해야함
+ */
+@Slf4j
+public class CheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex");        }
+    }
+}
